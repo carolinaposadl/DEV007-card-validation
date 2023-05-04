7042671a34c4cdbbc71fbf7be58b8b1eeb5da82b
c <carolinaposada@Carolinas-MacBook-Pro.local>
2023-05-03 22:25:59 -0500

    commit 1

--- src/index.js
+++ src/index.js
@@ -1,34 +1,18 @@
 import validator from './validator.js';
 
-// obteniendo los elementos del DOM
-const input = document.getElementById("creditCardNumber"); // obtener el input donde se ingresa el número de tarjeta desde index.html
-const validButton = document.getElementById("bottonValidar"); // obtener el botón Validar desde index.html
+// obteniendo elementos del DOM
+const input = document.getElementById("creditCardNumber"); // obteniendo el input donde se ingresa el número de tarjeta desde index.html
+const validButton = document.getElementById("bottonValidar"); // obteniendo el botón "Validar" desde index.html
 
 // ejecutando eventos del DOM
 validButton.addEventListener('click', () => {
-  validator.isValid(input.value); // ejecutando método isValid al dar click 
-  validator.maskify(input.value); // ejecutando método maskify al dar click
-
+  console.log(validator.isValid(input.value));
   // creando alert para ejecutar método en la interfaz
-  if (validator.isValid("creditCardNumber") === true) {
-    alert("Tarjeta válida");
+  if (validator.isValid(input.value) === true) {
+    alert("Tarjeta válida " + validator.maskify(input.value));
   } else {
     alert("Tarjeta inválida");
   }
 
-  //ejecutando maskify en la interfaz
-  (validator.maskify("creditCardNumber"));
-
   console.log(validator);
 });
-
-
-//traer el boolean de validator acá
-//si el resultado de la suma es multiplo de 10, entonces es true (la parte lógica iría en validator), de lo contrario es false
-//debo traer variable suma acá, como: validator.suma, quizá ()
-//pero además debo crear un alert desde acá que para un caso indique "Tarjeta válida" (true), o "Tarjeta inválida" (false)
-
-
-//APUNTES
-//toda la manipulación del html lo hago de mi index.js
-///en este archivo queremos llamar la función que está en validator.js
--- src/validator.js
+++ src/validator.js
@@ -1,27 +1,32 @@
 const validator = {
   isValid: function (creditCardNumber) {
     const validValue = creditCardNumber.split(""); // transformando string en array
-    const reversedValue = validValue.reverse(); // revertir el array (ahora hay índices)
+    const reversedValue = validValue.reverse(); // revertiendo el array (índices)
     console.log(reversedValue);
-    let sum = 0; // declarando variable suma para la suma final del array, si el resultado es multiplo de 10, suma será igual a 0
+    let sum = 0; // declarando variable "sum" para la suma final del array, si el resultado es multiplo de 10, "sum" será igual a 0
 
     for (let i = 0; i < reversedValue.length; i++) { //for declara i y se inicializa con el índice 0 - for es un ciclo que recorre el array
       if (((i + 1) % 2) === 0) // identificando las posiciones pares del array - el residual de la posición debe ser igual a 0 para determinar si es par
       {
-        reversedValue[i] = parseInt(reversedValue[i]) * 2 // multiplicando el valor del índice * 2, si la posición es par - parseando un texto a number 
+        reversedValue[i] = parseInt(reversedValue[i]) * 2 // multiplicando el valor del índice * 2 si la posición es par - parseando un texto a number 
         console.log(reversedValue[i])
         if (reversedValue[i] >= 10) // identificando si el resultado de la multiplicación es mayor o igual a 10
         {
-          //sacando el residuo del número y sumándolo a la parte entera - el Math.floor permite devolver la parte entera de un número
+          //tomando el residuo del número y sumándolo a la parte entera - Math.floor permite devolver la parte entera de un número
           reversedValue[i] = reversedValue[i] % 10 + Math.floor(reversedValue[i] / 10);
+          sum = sum + parseInt(reversedValue[i]); //sumando los números del array >=10
+        } else {
+          sum = sum + parseInt(reversedValue[i]); //sumando los demás números del array
         }
-        sum = sum + parseInt(reversedValue[i]); // sumando los valores del array
-      }
-      if (sum % 10 === 0) {
-        return true;
       } else {
-        return false;
+        sum = sum + parseInt(reversedValue[i]);
       }
+    }
+    //creando una condición para verificar la validez del número y la longitud del número ingresado
+    if (sum % 10 === 0 && creditCardNumber.length !== 0) {
+      return true;
+    } else {
+      return false;
     }
   },
 
@@ -35,47 +40,28 @@ const validator = {
 
 export default validator;
 
-
-
-//Preguntas:
-// ¿eslint es la herramienta que me permite leer los errores en el código?
+//
 
 
 
-//PASOS:
+
+
+
+
+
+
+
+
+
+
+
+//PASOS
 //Obtener la reversa del número a verificar .split
-//Transformando el input de string a array
+//Transformar el input de string a array
 //Identificar las posiciones pares del array
-//a todos los números que ocupan una posición par se les debe multiplicar por dos
-//si el resultado de la multiplicación es mayor o igual a 10
-//se deben sumar los digitos
-//finalmente sumar todos los números del array
-// el número a verificar será válido si la suma de sus dígitos finales es un múltiplo de 10. (alert) if
-//si no es invalida (alert) else
-//-> //hacer una división entre 10 - obtener el residual!!!!!! debe dar 0 (true), lo contrario es (false)
-
-//enviar el resultado a index.js - conectado a través del botón (?)
-
-
-//4083952015263
-
-
-//apuntes:
-//utilizar: funciones, variables, condiciones, ciclo for, objetos(métodos)
-//isValid(aqui va el input, pq es lo recibirá para poder analizarlo, ->
-//esta es una variable que se nombra, va a recibir algo del otro script)
-//lo que está en parentesis "vacio" es un parametro, el paremtro es la declaración de la variable
-
-
-//validator.isValid()
-//validator.maskify()
-
-// GUÍA:
-//const recibirMensaje = {
-//mensajeDelIndex(msj) {
-//alert('Mensaje recibido: ' + msj)
-//}
-//};
-//export default recibirMensaje;
-
-
+//Multiplicar *2 los números que ocupan una posición par
+//Sumar los dígitos si el resultado de la multiplicación es >= 10
+//Sumar los números impares
+//Sumar todos los números del array
+//Verificar si el resultado de la suma total es un número múltiplo de 10, if (true)
+//Verificar si no es múltiplo de 10, else (false)
